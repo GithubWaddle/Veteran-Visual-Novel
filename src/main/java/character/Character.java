5b1ed349f0d8f7623a2c007215ef4f5493fb280d
Tonychopper19 <faruq.alakhyar19@gmail.com>
2024-12-05 04:07:47 +0700

    Update Character.java

--- src/main/java/character/Character.java
+++ src/main/java/character/Character.java
@@ -1,5 +1,11 @@
 package main.java.character;
 
 public class Character {
+  public String name;
+    public java.util.Map<String, String> sprites;
 
+    public Character(String name) {
+        this.name = name;
+        this.sprites = new java.util.HashMap<>();
+    }
 }
