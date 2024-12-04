035cd3959a3bc6933250e2a4a77e7cca6e29bf75
AbdullahAhmadFaisal <abdullah085212ahmad@gmail.com>
2024-12-05 00:37:38 +0700

    Update Configuration.java

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,5 +1,8 @@
 package main.java;
 
 public class Configuration {
-
+    // Properti untuk konfigurasi program
+    public static final String STARTING_SCENE = "intro"; // Scene awal
+    public static final int WINDOW_WIDTH = 1080;        // Lebar window
+    public static final int WINDOW_HEIGHT = 720;       // Tinggi window
 }
