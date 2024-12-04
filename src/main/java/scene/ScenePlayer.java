082f896f5abd2e9f50c5654921aad5f97bf535d5
BeZeelBeast <157466868+BeZeelBeast@users.noreply.github.com>
2024-12-05 00:03:28 +0700

    Update ScenePlayer.java

--- src/main/java/scene/ScenePlayer.java
+++ src/main/java/scene/ScenePlayer.java
@@ -1,10 +1,60 @@
 package main.java.scene;
 
 import main.java.input.ClickSubscriber;
+import java.util.List;
 
 public class ScenePlayer implements ClickSubscriber {
+    private List<Scene> scenes;
+    private int currentSceneIndex;
+    private boolean isPlaying;
+    
+    public ScenePlayer(List<Scene> scenes) {
+        this.scenes = scenes;
+        this.currentSceneIndex = 0;
+        this.isPlaying = true;
+    }
+
     @Override
     public void mouseClicked() {
-        System.out.println("Mouse clicked!");
+        if (isPlaying) {
+            System.out.println("Mouse clicked!");
+            nextScene();
+        }
+    }
+
+    public void showScene() {
+        if (currentSceneIndex < scenes.size()) {
+            Scene currentScene = scenes.get(currentSceneIndex);
+            System.out.println("Menampilkan adegan: " + currentScene.getName());
+            System.out.println(currentScene.getContent());
+        } else {
+            System.out.println("Cerita selesai.");
+            isPlaying = false;
+        }
+    }
+
+    public void nextScene() {
+        if (currentSceneIndex < scenes.size() - 1) {
+            currentSceneIndex++;
+            showScene();
+        } else {
+            System.out.println("Sudah di adegan terakhir.");
+            isPlaying = false;
+        }
+    }
+
+    public void previousScene() {
+        if (currentSceneIndex > 0) {
+            currentSceneIndex--;
+            showScene();
+        } else {
+            System.out.println("Sudah di adegan pertama.");
+        }
+    }
+
+    public void play() {
+        currentSceneIndex = 0;
+        isPlaying = true;
+        showScene();
     }
 }
