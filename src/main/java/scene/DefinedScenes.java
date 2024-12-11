26883291e577df6a9b5eee1c3be9378a49c5f356
XenomLight <xenomlight@gmail.com>
2024-12-11 22:14:16 +0700

    Added xenom's add scene

--- src/main/java/scene/DefinedScenes.java
+++ src/main/java/scene/DefinedScenes.java
@@ -3,6 +3,7 @@ package main.java.scene;
 import main.java.actor.DefinedActors;
 import main.java.scene.events.*;
 import main.java.visual.actorsprite.ActorSprites;
+import main.java.visual.choicelist.ChoiceList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -53,7 +54,7 @@ public class DefinedScenes {
                     ),
                     new SceneEventShowCoverTransition(),
                     new SceneEventSwitchScene(
-                            "Home Thoughts of Decision"
+                            "Still Career Focus"
                     )
             )
         ));
@@ -111,7 +112,136 @@ public class DefinedScenes {
                 )
         ));
 
+        scenes.add(new Scene("Still Career Focus",
+                Arrays.asList(
+                        new SceneEventSetBackground("images/background/office2.png"),
+                        new SceneEventAddActorSprite(DefinedActors.getActorByName("Juan"),"default",ActorSprites.Position.BOTTOM_CENTER),
+                        new SceneEventDialog(".......",DefinedActors.getActorByName("Juan")),
+                        new SceneEventWait(500),
+                        new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"smile1"),
+                        new SceneEventDialog("Satu-satunya hal yang penting sekarang adalah karirku. Aku akan fokus total.",DefinedActors.getActorByName("Juan")),
+                        new SceneEventShowCoverTransition(),
+                        new SceneEventSwitchScene("Failed Career Focus")
 
+                )));
+
+        scenes.add(new Scene("Failed Career Focus",Arrays.asList(
+                new SceneEventSetBackground("images/background/empty_office.png"),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Bos Rafi"),"angry1",ActorSprites.Position.BOTTOM_RIGHT),
+                new SceneEventWait(500),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Bos Rafi"),"angry2"),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Juan"),"default",ActorSprites.Position.BOTTOM_LEFT),
+                new SceneEventDialog("Juan, proyek ini tidak berjalan sesuai harapan. Kami harus mengurangi beberapa posisi. Posisimu akan dihapuskan.",DefinedActors.getActorByName("Bos Rafi")),
+                new SceneEventWait(500),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"shocked"),
+                new SceneEventDialog("Ini sangat mengecewakan, Pak. Saya merasa sudah memberikan yang terbaik.",DefinedActors.getActorByName("Juan")),
+                new SceneEventWait(1000),
+                new SceneEventChoice(
+                        Map.of(
+                                "Kembali ke negara asal dan bertemu Clara",12,/*Return to clara*/
+                                "Mencoba bangkit membangun karir kembali di luar negeri",14/*Make it Big Outside*/
+                        )
+                ),
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("Return to Clara?"),
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("Make it Big Outside")
+
+        )));
+
+        scenes.add(new Scene("Return to Clara?",Arrays.asList(
+                new SceneEventSetBackground("images/background/park.png"),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Juan"),"default",ActorSprites.Position.BOTTOM_LEFT),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Clara"),"default",ActorSprites.Position.BOTTOM_RIGHT),
+                new SceneEventDialog(".......",DefinedActors.getActorByName("Clara")),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Clara"),"delighted2"),
+                new SceneEventDialog("Aku tahu kau sedang sulit, tapi aku masih percaya kita bisa saling mendukung. Aku ada di sini untukmu.",DefinedActors.getActorByName("Clara")),
+                new SceneEventWait(1000),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"sad"),
+                new SceneEventDialog("Aku tidak yakin apakah aku pantas untukmu sekarang.",DefinedActors.getActorByName("Juan")),
+                new SceneEventWait(1000),
+                new SceneEventChoice(
+                        Map.of(
+                                "Terima dukungan Clara",12,/*Love For Clara?*/
+                                "Tolak bantuan Clara",14/*New Happiness*/
+                        )
+                ),
+
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("Love For Clara?"),
+
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("New Happiness")
+
+                )));
+
+        scenes.add(new Scene("Make it Big Outside",Arrays.asList(
+                new SceneEventSetBackground("images/background/office2.png"),
+                new SceneEventWait(1000),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Juan"),"sad",ActorSprites.Position.BOTTOM_CENTER),
+                new SceneEventWait(100),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"default"),
+                new SceneEventWait(100),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"smile2"),
+                new SceneEventWait(100),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"smile1"),
+
+                new SceneEventDialog("Aku akan mencoba bangkit dan membangun karirku kembali.",DefinedActors.getActorByName("Juan")),
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("The Ending")
+
+        )));
+
+        scenes.add(new Scene("Love For Clara?",Arrays.asList(
+                new SceneEventSetBackground("images/background/busy_office.png"),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Juan"),"default",ActorSprites.Position.BOTTOM_LEFT),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Clara"),"angry1",ActorSprites.Position.BOTTOM_RIGHT),
+                new SceneEventDialog("..   .....   ......",DefinedActors.getActorByName("Clara")),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Clara"),"angry2"),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"shocked"),
+                new SceneEventDialog("Kau bahkan tidak punya waktu untuk makan malam bersama. Apa ini yang kau sebut hubungan?",DefinedActors.getActorByName("Clara")),
+                new SceneEventWait(1000),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"angry2"),
+                new SceneEventDialog("Aku melakukan ini untuk masa depan kita.",DefinedActors.getActorByName("Juan")),
+                new SceneEventWait(1000),
+                new SceneEventChoice(
+                        Map.of(
+                                "Kurangi fokus pada pekerjaan untuk menjaga hubungan",13,/*The Ending*/
+                                "Memprioritaskan pertumbuhan startup",15/*New Happiness*/
+                        )
+                ),
+
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("The Ending"),
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("New Happiness")
+
+        )));
+
+        scenes.add(new Scene("New Happiness",Arrays.asList(
+                new SceneEventSetBackground("images/background/park_night.png"),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Juan"),"sad",ActorSprites.Position.BOTTOM_LEFT),
+                new SceneEventDialog("..   ....   .......",DefinedActors.getActorByName("Juan")),
+                new SceneEventAddActorSprite(DefinedActors.getActorByName("Nanda"),"default",ActorSprites.Position.BOTTOM_RIGHT),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"default"),
+                new SceneEventDialog("Kadang, Juan, kita harus melepas sesuatu untuk menemukan kebahagiaan sejati.",DefinedActors.getActorByName("nanda")),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Juan"),"angry2"),
+                new SceneEventSetActorSpriteExpression(DefinedActors.getActorByName("Nanda"),"shocked"),
+                new SceneEventDialog("Tapi aku sudah melepas banyak hal, Nanda. Aku hanya ingin memulai lagi.",DefinedActors.getActorByName("Juan")),
+
+                new SceneEventChoice(
+                        Map.of(
+                                "Terima tawaran kerja di luar negeri",11,
+                                "Memulai hidup baru di kota sendiri",13
+                        )
+                ),
+
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("The Ending"),
+                new SceneEventShowCoverTransition(),
+                new SceneEventSwitchScene("The Ending")
+
+        )));
 
         // TEST
         scenes.add(new Scene(
@@ -255,6 +385,7 @@ public class DefinedScenes {
                         )
                 )
         ));
+
     }
 
     // Mendapatkan scene berdasarkan nama
